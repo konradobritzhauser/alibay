5db1788a0d5f4e3b2f623fdaf54d40690d1ab32d
konradobritzhauser <konradobritz@gmail.com>
2019-02-11 17:15:29 -0500

    finish signup endpoint

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Alibay
 
+
 [google doc](https://docs.google.com/document/d/1ZCAnrFAfK6et6a7iPMxvTqmCwYdaS5z-8UX1FG1NO8Y/edit
 )
+
+## Endpoints
+
+signup endpoint expects an object with username and password properties and their values. ex: {username:"dennis",password:"is cool"}
--- back/routes/user.js
+++ back/routes/user.js
@@ -9,10 +9,7 @@ setTimeout(() => {
   dbo = getdbo();
 }, 2000);
 
-setTimeout(() => {
-  dbo = getdbo();
-  console.log(dbo);
-}, 2000);
+
 
 router.post("/signup", (req, res) => {
   functionsList.logEPTrigger(req.originalUrl);
@@ -21,7 +18,10 @@ router.post("/signup", (req, res) => {
   console.log("username", username);
   let password = req.body.password;
   console.log("password", password);
-
+  dbo.collection('user').insertOne(req.body,(err,result)=>{
+    if(err) throw err
+    console.log('success')
+  })
   res.status(200).json({ message: "success" });
 });
 
