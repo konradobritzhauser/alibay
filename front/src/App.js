da81edd5a801a789eeef60155d6c2a650cd97dec
Jonathan <jonglbrt@gmail.com>
2019-02-11 13:43:13 -0500

    Added style.css, modified Navbar.js, added css link to index.html

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Navbar from "./components/Navbar";
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Navbar from './components/Navbar'
 
 class App extends Component {
   render() {
@@ -9,8 +9,8 @@ class App extends Component {
       <div>
         <Navbar />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -1,10 +1,11 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
+import '../css/style.css'
 
 class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
           <a class="navbar-brand" href="#">
             Navbar
           </a>
@@ -13,29 +14,57 @@ class Navbar extends Component {
             <ul class="navbar-nav mr-auto">
               <li class="nav-item">
                 <a class="nav-link" href="#">
-                  Link
+                  Buy
+                </a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Sell
                 </a>
               </li>
             </ul>
-            <form class="form-inline my-2 my-lg-0">
+
+            <form class="form-inline my-2 my-lg-0 mr-auto ml-auto">
               <input
                 class="form-control mr-sm-2"
+                id="navbar-search"
                 type="search"
                 placeholder="Search"
                 aria-label="Search"
               />
               <button
-                class="btn btn-outline-success my-2 my-sm-0"
+                class="btn btn-outline-secondary my-2 my-sm-0"
                 type="submit"
               >
                 Search
               </button>
             </form>
+
+            <ul class="navbar-nav justify-content-end">
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Login
+                </a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Signup
+                </a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  My Cart
+                </a>
+              </li>
+            </ul>
           </div>
         </nav>
       </div>
-    );
+    )
   }
 }
 
-export default Navbar;
+export default Navbar
